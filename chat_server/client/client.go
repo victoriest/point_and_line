5a9453f520140cce97f86a0a06163c69a0ca6790
victoriest <heest0205@gmail.com>
2014-06-29 17:40:54 +0800

    dev
    dev

--- chat_server/client/client.go
+++ chat_server/client/client.go
@@ -1,1 +1,78 @@
 package client
+
+import (
+	"../codec"
+	"../protocol"
+	"../utils"
+	"bufio"
+	log "code.google.com/p/log4go"
+	"fmt"
+	"net"
+	"strings"
+)
+
+// 消息处理托管
+type MessageRecivedHandler func(*RobotClient, *protocol.MobileSuiteModel)
+type MessageSenderHandler func(*RobotClient, *net.TCPConn, string)
+
+type IClient interface {
+	Startup()
+}
+
+type RobotClient struct {
+	port           string                // 服务端端口号
+	ip             string                // IP
+	quitSemaphore  chan bool             // 退出信号量
+	recivedHandler MessageRecivedHandler // 消息接收逻辑处理托管
+	sendHandler    MessageSenderHandler  // 消息发送逻辑处理托管
+	Probe          codec.ProtobufProbe   // 序列化实现
+}
+
+func NewClient(ip string, port string, recivedLogic MessageRecivedHandler, sendLogic MessageSenderHandler) *RobotClient {
+	client := new(RobotClient)
+	client.port = port
+	client.ip = ip
+	client.quitSemaphore = make(chan bool)
+	client.recivedHandler = recivedLogic
+	client.sendHandler = sendLogic
+	client.Probe = *new(codec.ProtobufProbe)
+	return client
+}
+
+func (self *RobotClient) Startup() {
+	strAddr := self.ip + ":" + self.port
+	tcpAddr, err := net.ResolveTCPAddr("tcp", strAddr)
+	utils.CheckError(err, true)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	utils.CheckError(err, true)
+	defer conn.Close()
+	log.Info("connecting ", conn.RemoteAddr().String(), "...")
+
+	go self.onSendMessage(conn)
+	go self.onMessageRecived(conn)
+
+	<-self.quitSemaphore
+}
+
+func (self *RobotClient) onSendMessage(conn *net.TCPConn) {
+	for {
+		var msg string
+		fmt.Scanln(&msg)
+
+		if strings.EqualFold(msg, "quit") {
+			self.quitSemaphore <- true
+			break
+		}
+		self.sendHandler(self, conn, msg)
+	}
+}
+
+func (self *RobotClient) onMessageRecived(conn *net.TCPConn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, _, err := self.Probe.DeserializeByReader(reader)
+		utils.CheckError(err, true)
+		self.recivedHandler(self, message)
+	}
+}
--- chat_server/victoriest_client.go
+++ chat_server/victoriest_client.go
@@ -1,16 +1,12 @@
 package main
 
 import (
+	"./client"
 	"./goconfig"
 	"./protocol"
-	estClient "./victoriest.org/client"
-	"./victoriest.org/probe"
-	"./victoriest.org/utils"
+	"./utils"
+	proto "code.google.com/p/goprotobuf/proto"
 	log "code.google.com/p/log4go"
-	// "image"
-	// "image/jpeg"
-	// "io"
-	// "io/ioutil"
 	"net"
 	"os"
 	"os/exec"
@@ -20,7 +16,7 @@ import (
 
 func main() {
 	ip, port := readServerConfig()
-	client := estClient.NewVictoriestClient(ip, port, msgReceivedHandler, msgSendHandler)
+	client := client.NewClient(ip, port, msgReceivedHandler, msgSendHandler)
 	client.Startup()
 }
 
@@ -39,63 +35,21 @@ func readServerConfig() (string, string) {
 	return host, port
 }
 
-func msgReceivedHandler(client *estClient.VictoriestClient, message *probe.VictoriestMsg) {
-	obj, _ := message.MsgContext.(map[string]interface{})
-	log.Debug(obj["ChatMessage"])
-
-	// if message.MsgType == 123 {
-	// 	f3, err := os.Create("123321.jpg")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer f3.Close()
-
-	// 	jpeg.Encode(f3, message.MsgContext.(image.Image), &jpeg.Options{90})
-	// }
-
-	// err := ioutil.WriteFile("temp.jpg", obj["ChatMessage"].([]byte), 0600)
-	// if err != nil {
-	// 	log.Error("err")
-	// }
+func msgReceivedHandler(client *client.RobotClient, message *protocol.MobileSuiteModel) {
+	// obj, _ := message.MsgContext.(map[string]interface{})
+	log.Info(message)
 }
 
-func msgSendHandler(client *estClient.VictoriestClient, writer *net.TCPConn, message string) {
-	jsonProbe := new(probe.JsonProbe)
-	chatMsg := protocol.ChatMsg{ChatMessage: message}
-	broMsg := &probe.VictoriestMsg{MsgType: protocol.MSG_TYPE_CHAT_MESSGAE, MsgContext: chatMsg}
-
-	strBuf, _ := jsonProbe.Serialize(broMsg)
-	writer.Write(strBuf)
-
-	// exefile, _ := exec.LookPath(os.Args[0])
-	// filepath := path.Join(filepath.Dir(exefile), "./123.jpg")
-	// file, err := os.Open(filepath)
-	// if err != nil {
-	// 	utils.CheckError(err, true)
-	// }
-
-	// m, _, err := image.Decode(file)
-	// mm := &probe.VictoriestMsg{MsgType: protocol.MSG_TYPE_CHAT_MESSGAE, MsgContext: m}
-	// strBuf, _ = jsonProbe.Serialize(mm)
-	// log.Debug(mm)
-
-	// var dest probe.VictoriestMsg
-	// jsonProbe.Deserialize(strBuf, &dest)
-
-	// f3, err := os.Create("123321.jpg")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f3.Close()
-	// jpeg.Encode(f3, mm.MsgContext.(image.Image), &jpeg.Options{50})
-
-	// err = ioutil.WriteFile("temp.jpg", b, 0600)
-	// if err != nil {
-	// 	log.Error("err")
-	// }
-
-	// broMsg := &probe.VictoriestMsg{MsgType: 123, MsgContext: m}
-	// strBuf, _ := jsonProbe.Serialize(broMsg)
-	// writer.Write(strBuf)
-
+func msgSendHandler(client *client.RobotClient, writer *net.TCPConn, message string) {
+	// probe := new(codec.ProtobufProbe)
+	testMessage := &protocol.ChatMsg{
+		ChatContext: proto.String(message),
+	}
+	byt, _ := proto.Marshal(testMessage)
+	msg := &protocol.MobileSuiteModel{
+		Type:    proto.Int32(123),
+		Message: byt,
+	}
+	bybuf, _ := client.Probe.Serialize(msg)
+	writer.Write(bybuf)
 }
