04ba38b7ff1b38c719eab40409e2ed65e1dbff67
卫锴 <weik@wenduing.com>
2018-08-08 14:08:22 +0800

    add config prop

--- go_server/victoriest_server.go
+++ go_server/victoriest_server.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strconv"
+
 	"./dao"
 	"./goconfig"
 	"./logic"
 	sev "./server"
 	"./utils"
 	log "github.com/alecthomas/log4go"
-	"os"
-	"os/exec"
-	"path"
-	"path/filepath"
 )
 
 func main() {
 	log.LoadConfiguration("./log4go.config")
-	port, ip, account, pwd, schame := readServerPort()
+	port, ip, dbPort, account, pwd, schame := readServerPort()
 	dbCon := new(dao.MysqlConnector)
-	isConnect := dbCon.Connect(&ip, 3306, &account, &pwd, &schame)
+	iDbPort, _ := strconv.Atoi(dbPort)
+	isConnect := dbCon.Connect(&ip, iDbPort, &account, &pwd, &schame)
 	if !isConnect {
 		log.Warn("mysql connect faild")
 		return
@@ -29,7 +32,7 @@ func main() {
 }
 
 // 读取配置文件
-func readServerPort() (string, string, string, string, string) {
+func readServerPort() (string, string, string, string, string, string) {
 	exefile, _ := exec.LookPath(os.Args[0])
 	log.Info(filepath.Dir(exefile))
 	filepath := path.Join(filepath.Dir(exefile), "./server.config")
@@ -39,11 +42,13 @@ func readServerPort() (string, string, string, string, string) {
 	utils.CheckError(err, true)
 	ip, err := cf.GetValue(goconfig.DEFAULT_SECTION, "db.ip")
 	utils.CheckError(err, true)
+	dbPort, err := cf.GetValue(goconfig.DEFAULT_SECTION, "db.port")
+	utils.CheckError(err, true)
 	account, err := cf.GetValue(goconfig.DEFAULT_SECTION, "db.user")
 	utils.CheckError(err, true)
 	pwd, err := cf.GetValue(goconfig.DEFAULT_SECTION, "db.pwd")
 	utils.CheckError(err, true)
 	schame, err := cf.GetValue(goconfig.DEFAULT_SECTION, "db.schame")
 	utils.CheckError(err, true)
-	return port, ip, account, pwd, schame
+	return port, ip, dbPort, account, pwd, schame
 }
